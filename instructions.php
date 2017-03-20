d8025f3760a5021cb4dff296b20419c8294b6575
mrurlwin <nz_danny_boy@hotmail.com>
2017-03-20 13:41:57 +1300

    Updated instructions

--- instructions.php
+++ instructions.php
@@ -74,16 +74,24 @@
                                 Add some dummy data for projects to be used for display.
                             </li>
                             <li>
-                                Then create a config.php file and connect the database up.
+                                Then create a config.php file and connect the database up. (Hint: connectDatabase() from Boogle)
                             </li>
                             <li>
-                                Create a foreach loop for the index page and loop over all projects in the database.
+                                Create a foreach loop for the index page and loop over all projects in the database. (Hint: getWebsites() from Boogle)
                             </li>
                             <li>
                                 Once done, wait for more instructions or add some fancy features of your own.
                             </li>
-
-
+                            <hr>
+                            <li>
+                                After successfully showing projects from the database, use the form in dashboard.php to insert projects into the table. (Hint: addFeedbackToDatabase() from Boogle)
+                            </li>
+                            <li>
+                                You have successfully completed the CR out of CRUD (Short for Create, Read, Update, Delete/Destroy) Google it for more information.
+                            </li>
+                            <li>
+                                Once done, wait for more instructions and add some PHP validation (Groan).
+                            </li>
                         </ul>
 
                         </div>
